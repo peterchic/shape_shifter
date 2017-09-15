3ad1d3cbff428ea7d9579a9df29a41c3d255e923
peterchic <peterchicarielli@gmail.com>
2017-09-15 14:55:00 -0400

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let elements = document.getElementsByName('cssProp')
+let div = document.getElementById('modify')
 
 function set(){
-  let div = document.getElementById('modify')
   for( let index = 0; index < elements.length; index++){
     let cssProp  = elements[index].getAttribute('id')
     let cssValue = elements[index].value
